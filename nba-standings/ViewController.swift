4c58c2f021be8fbb1d9753ad0626963ce04b59d4
tomalley104 <9475035+tomalley104@users.noreply.github.com>
2019-12-07 13:37:59 -0500

    Initial Commit

---
+++ nba-standings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nba-standings
+//
+//  Created by Tom OMalley on 12/7/19.
+//  Copyright © 2019 37th Street. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
